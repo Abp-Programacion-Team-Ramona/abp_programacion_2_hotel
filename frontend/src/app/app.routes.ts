12d84264ec972af7be7a27884c1d4e27d10ffd5d
DiegoMendia <diegomendia21@gmail.com>
2026-08-29 18:46:24 -0300

    Se agrega el componente not-found, error 404

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
 import { AdminDashboard } from './pages/admin-dashboard/admin-dashboard';
+import { NotFound } from './pages/not-found/not-found';
 
 export const routes: Routes = [
     { path: 'about-us', component: AboutUsComponent },
     { path: 'admin-dashboard', component: AdminDashboard },
+    { path: '**', component: NotFound }, // siempre al final
 ];
