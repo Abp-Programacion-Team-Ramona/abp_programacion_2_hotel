47516d4bf22fa22392e45a9b2690b30cae01e78e
Candela <candela.esperanza@hotmail.com>
2026-09-18 09:27:52 -0300

    feat(routing): configuro la ruta de registro en app.routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,12 +3,15 @@ import { AboutUsComponent } from './pages/about-us/about-us.component';
 import { AdminDashboard } from './pages/admin-dashboard/admin-dashboard';
 import { NotFound } from './pages/not-found/not-found';
 import { Home } from './pages/home/home';
+import { RegisterComponent } from './pages/register/register.component';
 import { ReservartionForm } from './pages/reservartion-form/reservartion-form';
+
 
 export const routes: Routes = [
     { path: 'about-us', component: AboutUsComponent },
     { path: 'admin-dashboard', component: AdminDashboard },
     { path: '', component: Home },
     { path: 'reservation-form', component: ReservartionForm },
+    { path: 'register', component: RegisterComponent},
     { path: '**', component: NotFound },
 ];
